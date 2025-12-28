8cb6d64bec31cf40facfe277fd84733f44269da3
jamie20040831 <jamie20040831@gmail.com>
2025-12-28 21:08:48 +0800

    Initial commit of personal website

---
+++ Intro.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <section id="intro">
+      <div className="container">
+        <div className="section-content">
+          <h2>Personal Intro</h2>
+          <p>Hello! I'm Yu-Chen, Chiu, a student in Department of Bioenvironmental Systems Engineering at National Taiwan University. I have a passion for exploring the world and discovering new experiences. In my free time, I enjoy traveling to new places and learning about different cultures, while also staying curious about new technologies.</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
